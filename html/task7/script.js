ef3f71908dee9cbfe191a871cfce44e4dee6448f
Prajal Maharjan <prajalmhrzn@gmail.com>
2025-06-23 16:05:23 +0545

    Task 7

---
+++ html/task7/script.js
@@ -0,0 +1,45 @@
+const forms = document.querySelectorAll("form");
+const backButtons = document.querySelectorAll(".back");
+console.log("Back buttons", backButtons);
+
+let activeForm = 2;
+forms[activeForm].style.display = "block";
+
+// for indicator
+const updateIndicator = () => {
+  const partitions = forms.length;
+
+  const indicatorElement = document.querySelector(".indicator");
+  const indicatorWidth = ((activeForm + 1) / partitions) * 100;
+
+  console.log("IndicatorWidth", indicatorWidth);
+  indicatorElement.style.width = indicatorWidth + "%";
+};
+updateIndicator();
+
+const updateForm = () => {
+  if (activeForm > forms.length - 1) return;
+  console.log("ActiveForm", activeForm);
+  forms.forEach((form) => {
+    form.style.display = "none";
+  });
+  forms[activeForm].style.display = "block";
+  updateIndicator();
+};
+updateForm();
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    activeForm++;
+    updateForm();
+  });
+});
+
+backButtons.forEach((backButton) => {
+  backButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    activeForm--;
+    updateForm();
+  });
+});
