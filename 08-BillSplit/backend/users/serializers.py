482f43eae29b28aa5bb222aaa14cfe31fb133464
Prajal Maharjan <prajalmhrzn@gmail.com>
2025-07-30 17:52:00 +0545

    feat: Bill-Spliiter User model login and signup

---
+++ 08-BillSplit/backend/users/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only = True)
+
+    class Meta:
+        model=User
+        fields=['id','email','name','password','profile_image']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already in use.")
+        return value
+
+    
+class UserSerializerBasic(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','email','name']
+
+class UserLoginSerializer(serializers.ModelSerializer):
+   
+   password = serializers.CharField(write_only=True)
+
+   class Meta:
+        model = User
+        fields = ['email','password']
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['name'] = user.name
+        return token
+
+    def validate(self, attrs):
+        attrs['username'] = attrs.get('email')
+        return super().validate(attrs)
